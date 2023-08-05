a6588c06a2e8ca546117141b9c514bee68aedbbb
rujuta13 <rujutabudke@gmail.com>
2023-08-05 13:23:16 +0530

    region rec

--- Backend/database_creation.py
+++ Backend/database_creation.py
@@ -12,7 +12,11 @@ cur = conn.cursor()
 cur.execute("""CREATE TABLE IF NOT EXISTS regionInfo(Name TEXT PRIMARY KEY,
     Status	TEXT,
     District	TEXT,
-    Population	TEXT)""")
+    Population	TEXT,
+    doctor_count INT,
+    doctor_visit DATE,
+    ngo_count INT,
+    ngo_visit DATE)""")
 
 cur.execute("""CREATE TABLE IF NOT EXISTS user (id TEXT(5) PRIMARY KEY,
         name TEXT NOT NULL,
@@ -29,6 +33,8 @@ cur.execute("""CREATE TABLE IF NOT EXISTS doctor(id TEXT(5) PRIMARY KEY,
         age INT, 
         gender CHAR(1),
         region TEXT REFERENCES regionInfo(name),
+        region2 TEXT ,
+        region3 TEXT ,
         contact INT,
         password TEXT NOT NULL);""")
 
@@ -36,6 +42,8 @@ cur.execute("""CREATE TABLE IF NOT EXISTS ngo(id TEXT(5) PRIMARY KEY,
         name TEXT NOT NULL,
         reg_no INT,
         region TEXT REFERENCES regionInfo(name),
+        region2 TEXT REFERENCES regionInfo(name),
+        region3 TEXT REFERENCES regionInfo(name),
         contact INT,
         password TEXT NOT NULL);""")
 
@@ -51,12 +59,12 @@ did = "D"+str(random.randint(1000,9999))
 nid = "N"+str(random.randint(1000,9999))
 
 #Adding data to the tables:
-'''
+
 cur.execute("""INSERT INTO user(id, name, age, gender, region, contact, password) VALUES(?,"User1", 20, 'F', "Pune", 1234567890, "password")""", (uid,))
 cur.execute("""INSERT INTO user(id, name, age, gender, region, contact, password) VALUES(?, "User2", 21, 'F', "Pune", 678904321, "password")""",(uid2,))
 
-cur.execute("""INSERT INTO doctor(id, name, qualification, reg_no, age, gender, region, contact, password) VALUES(?, "Doctor 1","MD Gyn", 12345, 41, 'F', "Pune", 904328761, "password")""",(did,))
-cur.execute("""INSERT INTO ngo(id, name, reg_no, region, contact, password) VALUES(?,"NGO 1", 54321, "Pune", 7891234560, "password")""", (nid,))
+cur.execute("""INSERT INTO doctor(id, name, qualification, reg_no, age, gender, region,  region2, region3,contact, password) VALUES(?, "Doctor 1","MD Gyn", 12345, 41, 'F', "Pune", "Chandrapur", "Bid", 904328761, "password")""",(did,))
+cur.execute("""INSERT INTO ngo(id, name, reg_no, region, region2, region3, contact, password) VALUES(?,"NGO 1", 54321, "Pune","Chandrapur", "Bid", 7891234560, "password")""", (nid,))
 
 periodData = pd.read_excel('Backend\SyntheticData.xlsx', header=0)  
 periodData.to_sql('periodLog', conn, if_exists='append', index=False)
@@ -64,8 +72,6 @@ periodData = pd.read_excel('Backend\RegularCycle.xlsx', header=0)
 periodData.to_sql('periodLog', conn, if_exists='append', index=False)
 regionData = pd.read_csv('Backend\Population.csv', header=0)  
 regionData.to_sql('regionInfo', conn, if_exists='append', index=False)
-
-'''
 
 conn.commit()
 
---
+++ Backend/doctor_rec.py
@@ -0,0 +1,34 @@
+import sqlite3
+
+conn = sqlite3.connect('Backend\PeriodTracker.db') #database path
+cur = conn.cursor()
+
+def region_rec(type):
+    '''
+    takes 'doctor' or 'ngo' as argument
+    returns list of regions
+    '''
+    regions = []
+
+    #######
+    
+    district1 = 'Pune'
+    district2 = 'Chandrapur'
+
+    if type == 'doctor':
+        #sort by DOCTOR COUNT
+        cur.execute("select name from regionInfo where district = ? OR district = ? order by doctor_count", (district1, district2))
+    else:
+        cur.execute("select name from regionInfo where district = ? OR district = ? order by ngo_count", (district1, district2))
+    
+    x = cur.fetchall()
+    for i in x[0:10]:
+        regions.append(i[0])
+
+    return regions
+
+lst1 = region_rec('ngo')
+print(lst1)
+lst2 = region_rec('doctor')
+print(lst2)
+
